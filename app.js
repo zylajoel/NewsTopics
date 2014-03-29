d087a1dfaacd7601e450b37feca141db89c194e0
Joel Zyla <joelzyla@gmail.com>
2014-03-28 20:35:47 -0400

    init

---
+++ app.js
@@ -0,0 +1,49 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+   , routes = require('./routes')
+   , http = require('http')
+   , path = require('path')
+   , app = express()
+   , logfmt = require("logfmt");
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(logfmt.requestLogger());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/data/googlenews', routes.googlenews);
+//app.get('/data/googlenews1', routes.googlenews1);
+// app.get('/quote/:id', function(req, res) {
+//   if(quotes.length <= req.params.id || req.params.id < 0) {
+//     res.statusCode = 404;
+//     return res.send('Error 404: No quote found');
+//   }
+
+//   var q = quotes[req.params.id];
+//   res.json(q);
+// });
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('newsTwist server listening on port ' + app.get('port'));
+});
