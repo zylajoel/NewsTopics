bea0c1bb6e2192c314e9b3f82340b663fe8dbbcb
Joel Zyla <joelzyla@gmail.com>
2014-04-06 21:58:47 -0400

    -passport local authentication 1. register new account 2. login
    * need to add captcha

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 
-/**
- * Module dependencies.
- */
-
 var express = require('express')
    , request = require('request')
    , cheerio = require('cheerio')
@@ -14,164 +10,129 @@ var express = require('express')
    , md5 = require('MD5')
    , mongo = require('mongodb')
    , passport = require('passport')
-   , LocalStrategy = require('passport-local').Strategy;
+   , LocalStrategy = require('passport-local').Strategy
+   , ParseNews = require('parse-news')
+   , crypto = require('crypto');
 
-// all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
+//app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'keyboard cat'}));
 app.use(logfmt.requestLogger());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
 
-
-// development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/datamonitor', routes.datamonitor);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('newsTwist server listening on port ' + app.get('port'));
-});
 
 
+/**
+ * User Authentication
+ */
 
-// Authenticate Users
-
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/login',
-                                   failureRedirect: '/poop' })
-);
-
-passport.use(new LocalStrategy({
-      emailField: 'email',
-      passwordField: 'passw',
-   },
-function (emailField, passwordField, done) {
-   process.nextTick(function () {
-      db.collection(users, function (error, collection) {
-         if (!error) {
-            collection.findOne({
-               'email': emailField,
-               'password': passwordField // use there some crypto function
-            }, function (err, user) {
-               if (err) {
-                  return done(err);
-               }
-               if (!user) {
-                  console.log('this email does not exist');
-                  return done(null, false);
-               }
-                  return done(null, user);
-            });
-         } else {
-            console.log(5, 'DB error');
-         }
-      });
-   });
-}));
-
-
-// Get news data and insert in database
 var uristring = 
   process.env.MONGOLAB_URI || 
   process.env.MONGOHQ_URL || 
   'mongodb://heroku_app23495772:6dv4rti8nij007hn89t0strkba@ds035557.mongolab.com:35557/heroku_app23495772';
 
-var timeInterval = setInterval(getNews, 1 * 60 * 1000);
-
-function getNews() {
-   console.log('');
-   console.log('**************************************')
-   console.log('******** Scraping Google News ********');
-
-   var url = 'http://news.google.com'
-      , json = {};
-
-   request(url, function(err, resp, body) {
-
-      if (err) {
-         throw err;
-      }
-
-      $ = cheerio.load(body);
-
-      json.stories = [];
-
-      $('.story').each(function() {
-
-         var theStory = $(this).find('.story .esc-lead-article-title-wrapper a.article .titletext').text() || "";
-
-         if (theStory.length) {
-
-            var story = {};
-
-            story.title = theStory;
-
-            story.href = $(this).find('.story .esc-lead-article-title-wrapper a.article').attr('url') || "";
-
-            story._id = md5(story.href);
-
-            story.source = $(this).find('.story .esc-lead-article-source-wrapper .al-attribution-source').text() || "";
-
-            story.intro = $(this).find('.story .esc-lead-snippet-wrapper').text() || "";
-
-            story.related = [];
-
-            $(this).find('.story .esc-extension-wrapper .esc-secondary-article-wrapper').each(function() {
-
-               var theRelatedStory  = $(this).find('.esc-secondary-article-title-wrapper a.article .titletext').text() || "";
-
-               if (theRelatedStory.length) {
-
-                  var relatedStory = {};
-
-                  relatedStory.title = theRelatedStory;
-
-                  relatedStory.href = $(this).find('.esc-secondary-article-title-wrapper a.article').attr('href') || "";
-
-                  story.related.push(relatedStory);
-               }
-            });
-
-            json.stories.push(story);
-         }
-      });
-
-      mongo.connect(uristring, function (err, db) {
-         if (err) { 
-            console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-         } else {
-            console.log ('Succeeded connected to: ' + uristring);
-            //db.collection('test', function(err, collection) {});
-            //db.createCollection('test', {w:1}, function(err, collection) {});
-            //db.collection('test', {w:1}, function(err, collection) {});
-
-            db.createCollection('stories', function(err, collection) {});
-           
-            var collection = db.collection('stories');
-
-            var doc2 = {'test2': 'doc2'};
-
-            var stories = json.stories;
-
-            //collection.insert(doc2, {w:1}, function(err, result) {});
-            collection.insert(stories, {continueOnError: true}, function(err, result) {
+passport.use(new LocalStrategy({usernameField: 'email', passwordField: 'password'}, function (userId, password, done) {
+   mongo.connect(uristring, function (err, db) {
+      db.collection("users", function(err, collection) {
+         if (!err) {
+            collection.findOne({
+               'email': userId
+            }, function (err, user) {
                if (err) {
-                  console.log('ERROR inserting stories.' + err);
-               } else {
-                  console.log('********Succeeded inserted stories********');
+                  return done(err);
                }
+               if (!user) {
+                  return done(null, false);
+               }
+               var iterations = 1000
+                 , keylen = 24
+                 , salt = user.salt;
+
+               var callback = function(err, key){
+                  var hexHash = Buffer(password, 'binary').toString('hex');
+                  if (hexHash !== user.password) {
+                     return done(null, false);
+                  } else {
+                     return done(null, user);
+                  }
+               };
+               crypto.pbkdf2(password, salt, iterations, keylen, callback);
             });
+         } else {
+            console.log(5, 'DB error');
          }
       });
-   });
-}
+   }); 
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+ 
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+
+
+/**
+ * Routes
+ */
+
+app.get('/', routes.index);
+
+app.get('/register', routes.registration);
+app.post('/register', routes.registerUser);
+
+app.get('/topics', loggedIn, routes.topics);
+
+app.post('/login', passport.authenticate('local'), function(req, res) {
+   res.redirect('/topics');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
+
+/**
+ * Functions
+ */
+
+// Check if user is logged in
+function loggedIn(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+};
+
+// Get news and insert in database
+ParseNews.google();
+
+
+
+/**
+ * Start server
+ */
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('newsTwist server listening on port ' + app.get('port'));
+});
--- routes/index.js
+++ routes/index.js
@@ -9,21 +9,73 @@ var request = require('request')
    , logfmt = require("logfmt")
    , md5 = require('MD5');
 
-exports.datamonitor = function(req, res) {
-   var url = 'http://news.google.com'
-   , mongoose = require ("mongoose")
-   , json = {};
-
-   request(url, function(err, resp, body) {
-
-      if (err) {
-         throw err;
-      }
-
-
-   });
-};
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index');
+};
+
+
+exports.topics = function(req, res) {
+   var json = {
+      'username': req.user.name
+   }
+   res.render('topics', json);
+};
+
+
+exports.registration = function(req, res){
+  res.render('register');
+};
+
+
+exports.registerUser = function(req, res){
+   var  mongo = require('mongodb')
+      , crypto = require ('crypto');
+
+   var user = {
+      "name": req.body.name,
+       "email": req.body.email,
+       "password": req.body.password
+   };
+
+   var uristring = 
+     process.env.MONGOLAB_URI || 
+     process.env.MONGOHQ_URL || 
+     'mongodb://heroku_app23495772:6dv4rti8nij007hn89t0strkba@ds035557.mongolab.com:35557/heroku_app23495772';
+
+
+  
+   var iterations = 1000;
+   var keylen = 24
+   var salt = crypto.randomBytes(128).toString('base64'); // salt
+  
+   var callback = function(err, key){
+      var hexHash = Buffer(user.password, 'binary').toString('hex');
+      // do something with the hashed password
+      user.password = hexHash;
+      user.salt = salt;
+   
+      mongo.connect(uristring, function (err, db) {
+         if (err) { 
+         } else {
+            var collection = db.collection('users');
+
+            collection.insert(user, function(err, result) {
+               if (err) {
+                  res.redirect('register');
+               } else {
+                  req.login(user, function(err) {
+                    if (err) {
+                      res.redirect('register');
+                    }
+                    return res.redirect('topics');
+                  });
+               }
+            });
+         }
+      });
+   };
+  
+  crypto.pbkdf2(user.password, salt, iterations, keylen, callback);
+
 };
